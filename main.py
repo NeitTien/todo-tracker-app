2fda5e6eb7ea0f62c24d62ff71203e17ced279a1
Tien Hoang <tienhoangxuan16@gmail.com>
2026-09-07 10:20:57 -0700

    Starting main file

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+#This create a window object to use Tkinter function
+window = tk.Tk()
+
+#This create title
+window.title("Limiter")
+
+#This create the resolution of the window
+window.geometry("1600x900")
+
+#This make the app stay opened and not instantly close after opening
+window.mainloop()
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 window = tk.Tk()
 window.title("Test Window")
-window.geometry("400x300")
+window.geometry("800x600")
 
 window.mainloop()
 
