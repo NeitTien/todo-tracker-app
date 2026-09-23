53206b7dc0221f89e5530b94dad2810e626c7ac5
Tien Hoang <tienhoangxuan16@gmail.com>
2026-09-23 09:26:38 -0700

    Also add boder to all frame for easy debug

--- calendar_view.py
+++ calendar_view.py
@@ -51,6 +51,10 @@ class CalendarView:
         self.create_button()
 
     def create_widgets(self):
+        #Expand the Parent Frame
+        self.parent.grid_columnconfigure(0, weight=1)
+        self.parent.grid_rowconfigure(0, weight=1)
+
         #Calendar View
         self.calendar_view = tk.Frame(
             self.parent,
@@ -63,19 +67,44 @@ class CalendarView:
             sticky="nsew"
         )
 
+        #Calendar scaling configuration
+        #This is needed so that header and grid can expand
+        self.calendar_view.grid_columnconfigure(0, weight=1)
+        self.calendar_view.grid_rowconfigure(0, weight=0) #Fixed for Header
+        self.calendar_view.grid_rowconfigure(1, weight=1) #Grid
+
         #Calendar Header
-        self.calendar_header = tk.Frame(self.calendar_view)
+        self.calendar_header = tk.Frame(
+            self.calendar_view,
+            highlightbackground="yellow",
+            highlightthickness=1
+        )
         self.calendar_header.grid(
             row=0,
-            column=0
+            column=0,
+            sticky="nsew"
         )
+        #Header scaling configuration
+        self.calendar_header.grid_columnconfigure(0, weight=1)
+        self.calendar_header.grid_rowconfigure(0, weight=1)
 
         #Calendar Grid
-        self.calendar_grid = tk.Frame(self.calendar_view)
+        self.calendar_grid = tk.Frame(
+            self.calendar_view,
+            highlightbackground="black",
+            highlightthickness=1
+        )
         self.calendar_grid.grid(
             row=1,
-            column=0
+            column=0,
+            sticky="nsew"
         )
+
+        #Grid scaling configuration
+        for column in range(7):
+            self.calendar_grid.grid_columnconfigure(column, weight=1)
+        for row in range(6):
+            self.calendar_grid.grid_rowconfigure(row, weight=1)
 
     def create_label(self):
         #Month Label
@@ -105,7 +134,7 @@ class CalendarView:
             label = tk.Label(
                 self.calendar_header,
                 text=day,
-                width=11,
+                #width=11,
                 font=("Arial", 12)
             )
             label.grid(
@@ -120,34 +149,55 @@ class CalendarView:
         day_of_week = len(self.current_day_of_month[0])
         #For every week in a month
         for week in range(0, week_size):
-            row = week
             #For every day in a week
             for day in range(0, day_of_week):
-                column = day
+                day_obj = self.current_day_of_month[week][day]
 
                 #This will make days that is not of current month grayed out
                 #696969 is gray color and FFFFFF is white color
                 #add foreground if want to change text_color
-                if self.current_day_of_month[week][day].month != self.current_month:
+                if day_obj.month != self.current_month:
                     background_color = "#696969"
+                    text_color = "#CCCCCC"
                 else:
                     background_color = "#FFFFFF"
+                    text_color = "#000000"
                 
-                day_label = tk.Label(
+                #Frame for each day
+                day_frame = tk.Frame(
                     self.calendar_grid,
-                    text=str(self.current_day_of_month[week][day].day),
-                    font=("Arial", 12),
-                    width=11,
-                    height=5,
-                    relief="solid",
-                    background = background_color
+                    background=background_color,
+                    highlightbackground="#D0D0D0",
+                    highlightthickness=1,
+                    bd=0
+                )
+                day_frame.grid(
+                    row=week,
+                    column=day,
+                    sticky="nsew",
+                    padx=1,
+                    pady=1
                 )
 
+                day_frame.grid_columnconfigure(0, weight=1)
+                day_frame.grid_rowconfigure(1, weight=1)
+                #Prevent the frame from auto shrinking to fits the content inside
+                #day_frame.grid_propagate(False) 
+
+                day_label = tk.Label(
+                    day_frame,
+                    text=str(day_obj.day),
+                    font=("Arial", 12),
+                    background = background_color,
+                    foreground = text_color,
+                    anchor="ne"
+                )
                 day_label.grid(
-                    row=row,
-                    column=column,
-                    padx=2,
-                    pady=2
+                    row=0,
+                    column=0,
+                    sticky="ne",
+                    padx=5,
+                    pady=5
                 )
 
     
@@ -226,31 +276,60 @@ class CalendarView:
 
         week_size = len(self.current_day_of_month)
         day_of_week = len(self.current_day_of_month[0])
+
+        #Expand column and row of each day
+        for row in range(week_size):
+            self.calendar_grid.grid_rowconfigure(row, weight=1, uniform="row")
+        for column in range(day_of_week):
+            self.calendar_grid.grid_columnconfigure(column, weight=1, uniform="col")
+
         for week in range(0, week_size):
-            row = week
             for day in range(0, day_of_week):
-                column = day
-                            
-                if self.current_day_of_month[week][day].month != self.current_month:
+                day_obj = self.current_day_of_month[week][day]
+
+                if day_obj.month != self.current_month:
                     background_color = "#696969"
+                    text_color = "#CCCCCC"
                 else:
                     background_color = "#FFFFFF"
+                    text_color = "#000000"
+
+                #Frame for each day
+                day_frame = tk.Frame(
+                    self.calendar_grid,
+                    background=background_color,
+                    highlightbackground="#D0D0D0",
+                    highlightthickness=1,
+                    bd=0
+                )
+                day_frame.grid(
+                    row=week,
+                    column=day,
+                    sticky="nsew",
+                    padx=1,
+                    pady=1
+                )
+
+                day_frame.grid_columnconfigure(0, weight=1)
+                day_frame.grid_rowconfigure(1, weight=1)
+                #Prevent the frame from auto shrinking to fits the content inside
+                #day_frame.grid_propagate(False) 
 
                 day_label = tk.Label(
-                    self.calendar_grid,
-                    text=str(self.current_day_of_month[week][day].day),
+                    day_frame,
+                    text=str(day_obj.day),
                     font=("Arial", 12),
-                    width=11,
-                    height=5,
-                    relief="solid",
-                    background = background_color
+                    background = background_color,
+                    foreground = text_color,
+                    anchor="ne"
                 )
-
                 day_label.grid(
-                    row=row,
-                    column=column,
-                    padx=2,
-                    pady=2
+                    row=0,
+                    column=0,
+                    sticky="ne",
+                    padx=5,
+                    pady=5
                 )
+                
     
 
